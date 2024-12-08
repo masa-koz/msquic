8a5f306cdd79280d490caaa6f8b0e40b8c02e2a8
Masahiro Kozuka <masa.koz@kozuka.jp>
2024-12-08 11:42:07 +0900

    Add macOS framework linking for static builds in build script

--- scripts/build.rs
+++ scripts/build.rs
@@ -45,6 +45,9 @@ fn main() {
     let lib_path = Path::join(Path::new(&dst), Path::new(path_extra));
     println!("cargo:rustc-link-search=native={}", lib_path.display());
     if cfg!(feature = "static") {
+        if cfg!(macos) {
+            println!("cargo:rustc-link-arg=-framework CoreFoundation -framework Security");
+        }
         println!("cargo:rustc-link-lib=static=msquic");
     }
 }
